21b6fbb3a45bdd37dfc1144f7208acfe94b77fd5
kevinongg <kevinongg@gmail.comm>
2024-10-09 12:51:20 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Vault Combination 10 - 40 - 39
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Calculation for the first code
+const code1 = (6 + 4) * 1; // Using addition and multiplication
+
+// Calculation for the second code
+const code2 = (10 * 4) + 0; // Using multiplication and addition
+
+// Calculation for the third code
+const code3 = (100 / 2) - 11; // Using division and subtraction
+
+alert(`${message} The combination is: ${code1} - ${code2} - ${code3}`)
